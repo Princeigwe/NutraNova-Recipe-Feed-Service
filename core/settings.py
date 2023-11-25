4ff4226aa0dc04241090d36c70a408d88cf62c8c
Prince Igwe <igwep297@gmail.com>
2023-11-25 06:33:53 +0100

    installed django rest framework for recipe image upload with REST API

--- core/settings.py
+++ core/settings.py
@@ -43,11 +43,13 @@ INSTALLED_APPS = [
 
     #local apps
     "recipes.apps.RecipesConfig",
+    "file.apps.FileConfig",
 
 
     # 3rd party apps
     "ariadne_django",
     "cloudinary",
+    "rest_framework",
 ]
 
 MIDDLEWARE = [
@@ -137,6 +139,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

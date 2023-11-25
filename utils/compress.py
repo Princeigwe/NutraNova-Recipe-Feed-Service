78d1bef7186f46305905bf9ecdd8c6032528993c
Prince Igwe <igwep297@gmail.com>
2023-11-25 05:53:02 +0100

    comression works, still checking something

--- utils/compress.py
+++ utils/compress.py
@@ -1,13 +1,23 @@
 from PIL import Image
 import os
 from dotenv import load_dotenv
+from django.conf import settings
 
 load_dotenv()
 
 
 def compress_image(image):
-  image_path = os.path.join(os.getcwd().image)
+  # access the image file
+  image_path = os.path.join(os.getcwd(), image)
+  print(image)
+  print(image_path)
   image = Image.open(image_path)
-  compressed_image = image.save(f"compressed_{image}", optimize=True, quality=20)
-  print(compressed_image)
-  return compressed_image
+  width, height = image.size
+
+  new_size = (width//2, height//2)
+  resized_image = image.resize(new_size)
+  
+  # replacing the original image with the compressed image
+  resized_image.save(image_path, 'JPEG', optimize=True, quality=90)
+  # return image_path # the new compressed image
+  return 1

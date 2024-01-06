f677c969c6714c3dacbaad6582c964bfb6bd7900
Prince Igwe <igwep297@gmail.com>
2024-01-06 18:02:28 +0100

    recipe feed resolver now working in asgi server

--- recipes/resolvers.py
+++ recipes/resolvers.py
@@ -209,6 +209,7 @@ def resolve_edit_recipe(_, info, input):
 # cache the response for 15 minutes
 # repeat from 1 and 2 when user makes another recipe feed request
 
+@database_sync_to_async
 def resolve_recipe_feed(_, info):
   """
   The `resolve_recipe_feed` function retrieves a user's recipe feed from the cache, and if it doesn't

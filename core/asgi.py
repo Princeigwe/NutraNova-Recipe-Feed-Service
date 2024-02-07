68332ab6c747c17fded117a0ee22eba0d37a7608
Prince Igwe <igwep297@gmail.com>
2024-02-07 20:11:01 +0100

    not really sure why this isnt working on remote envinronment

--- core/asgi.py
+++ core/asgi.py
@@ -24,12 +24,12 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 # application = get_asgi_application()
 
 # wrapping SessionMiddlewareStack enabled request session in ASGI server
-# application = SessionMiddlewareStack( 
-#     URLRouter([
-#       path("graphql/", GraphQL(schema=schema, websocket_handler=GraphQLTransportWSHandler(), debug=True)),
-#       re_path(r"", get_asgi_application())
-#     ])
-#   )
+application = SessionMiddlewareStack( 
+    URLRouter([
+      path("graphql/", GraphQL(schema=schema, websocket_handler=GraphQLTransportWSHandler(), debug=True)),
+      re_path(r"", get_asgi_application())
+    ])
+  )
 
 
 # fix for:  ValueError: Django can only handle ASGI/HTTP connections, not websocket.
@@ -48,9 +48,9 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 
 
-application = URLRouter([
-      path("graphql/", GraphQL(schema=schema, websocket_handler=GraphQLTransportWSHandler(), debug=True)),
-      re_path(r"", get_asgi_application())
-    ])
+# application = URLRouter([
+#       path("graphql/", GraphQL(schema=schema, websocket_handler=GraphQLTransportWSHandler(), debug=True)),
+#       re_path(r"", get_asgi_application())
+#     ])
 
 # app = application

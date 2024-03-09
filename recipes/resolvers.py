5060a17eb47c04892f4ec6b53be6e1feb0f2d573
Prince Igwe <igwep297@gmail.com>
2024-03-09 01:07:13 +0100

    sent kafka message for deleting like relationship when un-liking recipe

--- recipes/resolvers.py
+++ recipes/resolvers.py
@@ -15,6 +15,7 @@ import time
 from threads.like_recipe_thread import LikeRecipeThread
 from utils.kafka.produce.create_neo_graph_nodes import send_graph_nodes_details
 from utils.kafka.produce.create_neo_graph_chef_like_recipe_rel import send_chef_like_recipe_details
+from utils.kafka.produce.delete_neo_graph_chef_like_recipe_rel import send_delete_chef_like_recipe_details
 
 # todo: do not add the database_sync_to_async decorator, since this function is not being resolved
 def get_tag(name):
@@ -430,6 +431,15 @@ def resolve_unlike_recipe(_, info, pk):
     chef = Chef.objects.get(username=user['username'])
     like = Like.objects.get(recipe=recipe, liker=chef)
     like.delete()
+
+    # send message to kafka
+    kafka_message = {
+      "chef_username": recipe.chef.username,
+      "recipe_title": recipe.title,
+      "recipe_published": str(recipe.published),
+    }
+
+    send_delete_chef_like_recipe_details(kafka_message)
     return f"You un-liked recipe by {recipe.chef.username}"
   
   except Recipe.DoesNotExist as error:

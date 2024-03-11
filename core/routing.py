5cd92cc849d8ae036df8ff9f21918d8c6369032e
Prince Igwe <igwep297@gmail.com>
2024-03-11 17:37:44 +0100

    minor change for Apollo subscription configuration

--- core/routing.py
+++ core/routing.py
@@ -7,6 +7,10 @@ from django.core.asgi import get_asgi_application
 
 
 websocket_urlpatterns = [
-  path("graphql/", GraphQL(schema=schema, websocket_handler=GraphQLTransportWSHandler(), debug=True)),
+  # normal deployment setting
+  # path("graphql/", GraphQL(schema=schema, websocket_handler=GraphQLTransportWSHandler(), debug=True)),
+
+  # setting for subscription on Apollo GraphOS
+  path("ws/", GraphQL(schema=schema, websocket_handler=GraphQLTransportWSHandler(), debug=True)),
   re_path(r"", get_asgi_application()),
 ]

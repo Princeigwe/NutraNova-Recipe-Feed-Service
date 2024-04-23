da40607a208abcfd35b9358ba934016eeb9b380a
Prince Igwe <igwep297@gmail.com>
2024-04-23 23:05:27 +0100

    refactor on resolver for all current user drafts

--- recipes/resolvers.py
+++ recipes/resolvers.py
@@ -398,11 +398,30 @@ def resolve_my_drafts(_, info):
   try:
     chef = Chef.objects.get(username=user['username']) 
 
+    drafts_responses = []
+    # retrieve all tags manay-to-many field for draft in a single fetch 
+    recipe_drafts = chef.recipes.filter(status='DRAFT').prefetch_related('tags')
+    for draft in recipe_drafts:
+      draft_response = model_to_dict(draft, exclude=['tags', 'created', 'published', 'chef'])
+      print("single draft: ", draft_response)
+      chef_details = {
+            "username": chef.username,
+            "first_name": chef.first_name,
+            "last_name": chef.last_name
+          }
+      draft_response['tags'] = [tag.name for tag in draft.tags.all()]
+      draft_response['created'] = draft.created
+      draft_response['published'] = draft.published
+      draft_response['chef'] = chef_details
+
+      drafts_responses.append(draft_response)
+    return drafts_responses
+
+    #* not sure this comment is necessary anymore, but I'll leave it
     # recipe query gave the synchronous only operation error even when using the channels database_sync_to_async decorator.
     # wrapping it in list function, retrieved the recipes properly.
     # reference to: https://stackoverflow.com/questions/63149616/getting-synchronousonlyoperation-error-even-after-using-sync-to-async-in-django
-    drafts = list(chef.recipes.filter(status='DRAFT')) 
-    return drafts
+
   except Chef.DoesNotExist:
     raise Exception("Chef data does not exist")
   except Recipe.DoesNotExist:
@@ -414,7 +433,7 @@ def resolve_draft(_, info, pk):
   user = get_user(info)
   try:
     chef = Chef.objects.get(username=user['username'])
-    draft = chef.recipes.get(pk=pk, status="DRAFT")
+    draft = chef.recipes.get(pk=pk, status="DRAFT").__dict__
     return draft
   
   except Chef.DoesNotExist:

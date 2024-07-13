823575aa56b4f42f5464c18186611afa3b1f8f05
Prince Igwe <igwep297@gmail.com>
2024-07-13 12:15:06 +0100

    story expiration refactored

--- utils/expired_stories.py
+++ utils/expired_stories.py
@@ -13,15 +13,17 @@ def fetch_expired_stories():
   current_epoch_time = calendar.timegm(current_time.timetuple())
   print("current epoch time: ", current_epoch_time)
 
-  all_stories = stories.find()
-  for story in all_stories:
-    story_date = story.date
-    story_expiry_date = story_date + timedelta(days=1)
-    story_expiry_epoch_time = calendar.timegm(story_expiry_date.timetuple())
-    print(story_expiry_epoch_time)
+  if stories.count_documents({}) >= 0:
+    all_stories = stories.find()
+    for story in all_stories:
+      story_date = story['date']
+      story_expiry_date = story_date + timedelta(days=1)
+      story_expiry_epoch_time = calendar.timegm(story_expiry_date.timetuple())
+      print(story_expiry_epoch_time)
 
-    if story_expiry_epoch_time >= current_epoch_time:
-      expired_stories.append(story)
+      # if story_expiry_epoch_time >= current_epoch_time:
+      if current_epoch_time >= story_expiry_epoch_time:
+        expired_stories.append(story)
   
   return expired_stories
 
@@ -32,7 +34,7 @@ def delete_expired_stories():
 
   print("fetched expired stories: ", fetch_expired_stories)
   for story in fetched_expired_stories:
-    stories.delete_one({"_id": story._id})
+    stories.delete_one({"_id": story['_id']})
 
   print("Expired stories deleted")
   print("fetched expired stories: ", fetch_expired_stories)

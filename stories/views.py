5e66a423cf78e075b5778ff43eb053d6ab312dbd
Prince Igwe <igwep297@gmail.com>
2024-07-22 19:02:46 +0100

    base64 media encoding

--- stories/views.py
+++ stories/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .mongo_database import database
-from utils.request_authz import jwt_authorization
-from rest_framework.decorators import api_view
 from utils.get_user import get_user_rest
 import datetime
 import os
+import base64
+from PIL import Image
+from django.conf import settings
+import time
 
 
 stories = database.recipe_stories
@@ -18,36 +20,58 @@ def upload_story(request, image_path):
   saved in MongoDB, and the file is deleted from the code filesystem.
   """
   user = get_user_rest(request)
-  file = open(image_path, "rb")
-  file_content = file.read()
-  username = user['username']
-  story = {
-    "username": username,
-    "image": file_content,
-    "date": datetime.datetime.now(tz=datetime.timezone.utc)
-  }
-  story = stories.insert_one(story)
-  # delete image file from media directory after upload
-  os.remove(image_path)
-  print(story)
+
+  #* SOLUTION ON HOW TO SAVE MEDIA FILES IN MONGODB AND EFFECTIVELY REPRESENT THAT DATA IN GRAPHQL DATA SCHEMA:
+  #todo 1: when file is selected, convert to webp to reduce size and maintain quality.
+  #todo 2: compress it further to 1mb to retain all data without loss when encoding.
+  #todo 3: read the file buffer data and encode it in base64
+  #todo 4: save the encoded string to mongodb with the attribute "base64_encoded"
+  #todo 5: to represent this data in GraphQL schema, use the String data type
+  if image_path.endswith(".jpg") or image_path.endswith(".jpeg") or image_path.endswith(".png"):
+
+    default_media_path = settings.MEDIA_ROOT  # media directory
+    image_name_without_extension = os.path.splitext(os.path.basename(image_path))[0]
+    image = Image.open(image_path)
+    image = image.convert('RGB')
+    directed_path = f"{default_media_path}/" + f"{image_name_without_extension}" + ".webp"
+    image.save(directed_path, "webp")
+
+    image_size = os.path.getsize(directed_path)
+
+    limit_size = 5242880 #5MB
+
+    # as long as the webp image is more than 5MB, compress it
+    while image_size > limit_size:
+      image = Image.open(image_path)
+      width, height = image.size
+
+      new_size = (width//2, height//2)
+      resized_image = image.resize(new_size)
+      
+      # replacing the previous webp image with the compressed image
+      resized_image.save(directed_path, 'WEBP', quality=90)
+
+    file = open(directed_path, "rb")
+    file_content = file.read()
+
+    encoded_image = base64.b64encode(file_content).decode('utf-8')
+    username = user['username']
+    story = {
+      "username": username,
+      "encoded": encoded_image,
+      "date": datetime.datetime.now(tz=datetime.timezone.utc)
+    }
+    story = stories.insert_one(story)
+    
+    # delete image files from media directory after upload
+    os.remove(image_path)
+    os.remove(directed_path)
   
+  else:
+    # deleting the invalid file
+    os.remove(image_path)
+    raise Exception("Invalid file type. Upload image")
 
 
-def get_followings_stories(request):
-  """get stories from the users that the current user is following"""
-  pass
-
-
-def get_following_single_story(request):
-  """get a single story for a user that the current user is following"""
-  pass
-
-
-def get_my_stories(request):
-  pass
-
-
-def get_my_story(request):
-  pass
 
 

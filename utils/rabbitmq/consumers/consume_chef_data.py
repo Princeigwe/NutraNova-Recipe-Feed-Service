2592f249304c202ae597c34a2f6fd5315fb5c595
Prince Igwe <igwep297@gmail.com>
2024-12-25 02:34:29 +0100

    rabbitmq message type consumption

--- utils/rabbitmq/consumers/consume_chef_data.py
+++ utils/rabbitmq/consumers/consume_chef_data.py
@@ -3,7 +3,11 @@ from dotenv import load_dotenv
 load_dotenv()
 import json
 from django.core.cache import cache
-from utils.rabbitmq.rabbitmq_config import channel
+# from utils.rabbitmq.rabbitmq_config import channel
+from utils.rabbitmq.channels.consume_chef_data_channel import channel
+import os
+
+rabbitmq_message_type = os.environ.get('CHEF_DATA_UPDATE_MESSAGE_TYPE')
 
 
 ##** this queue was manually created on the dashboard
@@ -17,32 +21,33 @@ result = channel.queue_declare(queue=queue, exclusive=True) # 'exclusive' argume
 channel.queue_bind(exchange=exchange_name, queue=result.method.queue)
 
 def consume_and_update_chef_data(message):
-  try:
-    print(f"Received message: {message}")
+  if message['type'] == rabbitmq_message_type:
+    try:
+      print(f"Received message: {message}")
 
-    from recipes.models import Chef
+      from recipes.models import Chef
 
-    if 'old_username' in message:
-      chef = Chef.objects.get(username=message['old_username'])
-      chef.username = message['new_username']
-      chef.save()
-      print(f"{chef.username} username updated")
+      if 'old_username' in message:
+        chef = Chef.objects.get(username=message['old_username'])
+        chef.username = message['new_username']
+        chef.save()
+        print(f"{chef.username} username updated")
+      
+      # this is a response operation for the 'updateProfile' resolver in the user microservice
+      else:
+        chef = Chef.objects.get(username=message['username'])
+        chef.image = message['image'] if 'image' in message else chef.image
+        chef.first_name = message['first_name'] if 'first_name' in message else chef.first_name
+        chef.last_name = message['last_name'] if 'last_name' in message else chef.last_name
+        chef.vote_strength = message['vote_strength'] if 'vote_strength' in message else chef.vote_strength
+        chef.is_verified = message['is_verified'] if 'is_verified' in message else chef.vote_strength
+        chef.save()
+        print(f"{chef.username} data updated")
     
-    # this is a response operation for the 'updateProfile' resolver in the user microservice
-    else:
-      chef = Chef.objects.get(username=message['username'])
-      chef.image = message['image'] if 'image' in message else chef.image
-      chef.first_name = message['first_name'] if 'first_name' in message else chef.first_name
-      chef.last_name = message['last_name'] if 'last_name' in message else chef.last_name
-      chef.vote_strength = message['vote_strength'] if 'vote_strength' in message else chef.vote_strength
-      chef.is_verified = message['is_verified'] if 'is_verified' in message else chef.vote_strength
-      chef.save()
-      print(f"{chef.username} data updated")
-  
-  except Chef.DoesNotExist:
-    pass
-  except KeyboardInterrupt:
-    pass
+    except Chef.DoesNotExist:
+      pass
+    except KeyboardInterrupt:
+      pass
 
 
 def callback(ch, method, properties, body):

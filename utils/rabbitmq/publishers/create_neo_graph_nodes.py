2194a7821bd648a59b1535bc7b256fa943a3dcbc
Prince Igwe <igwep297@gmail.com>
2024-12-29 12:01:14 +0100

    created a separate channel for this publisher

--- utils/rabbitmq/publishers/create_neo_graph_nodes.py
+++ utils/rabbitmq/publishers/create_neo_graph_nodes.py
@@ -1,8 +1,10 @@
 import logging
 import json
 from pika.exceptions import AMQPError
-from utils.rabbitmq.rabbitmq_config import channel, exchange_name
+from utils.rabbitmq.channels.publish_published_recipe_channel import channel
+import os
 
+exchange_name=os.environ.get('CLOUDAMQP_FANOUT_EXCHANGE')
 def send_graph_nodes_details(message: dict):
   try:
     channel.basic_publish(exchange=exchange_name, routing_key='', body=json.dumps(message)) # publishing to fanout exchange
